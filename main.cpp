56f2ba603eed7ae92fd518ddf9e7c3d9992744a0
agata <e12023151@student.tuwien.ac.at>
2025-03-01 02:54:45 +0100

    Index Buffer

--- main.cpp
+++ main.cpp
@@ -28,12 +28,26 @@ int main() {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLfloat vertices[] = {
-	-1.0f,-1.0f,0.0f,
-	0.0f,0.0f,0.0f,
-	0.7f, -1.0f, 0.0f };
 
-	GLFWwindow* window = glfwCreateWindow(800, 800, "fractals", NULL, NULL);
+	GLfloat vertices[] =
+	{
+		-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower left corner
+		0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, // Lower right corner
+		0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, // Upper corner
+		-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner left
+		0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, // Inner right
+		0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f // Inner down
+	};
+
+	// Indices for vertices order
+	GLuint indices[] =
+	{
+		0, 3, 5, // Lower left triangle
+		3, 2, 4, // Upper triangle
+		5, 4, 1 // Lower right triangle
+	};
+
+	GLFWwindow * window = glfwCreateWindow(800, 800, "fractals", NULL, NULL);
 
 	if (window == NULL) {
 		std::cout << "Failed to create window" << std::endl;
@@ -79,18 +93,23 @@ int main() {
 	//vao needs to be generated before vbo
 
 	//refereence container for vao and vbo
-	GLuint VAO, VBO;
+	GLuint VAO, VBO, EBO;
 
 	//1 object each
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	//make VAO the current vao by binding it
 	glBindVertexArray(VAO);
 
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
 	//store verticies in the vbo
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
 	//configures vertex attr so that opengl knows how to read the VBO
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
@@ -98,10 +117,10 @@ int main() {
 	//enable vertex attr so opengl knows how to use it
 	glEnableVertexAttribArray(0);
 
-	//bind VBO and VAO to 0 so that we dont accidentally modify the VAO and VBO we created
+	//bind VBO and VAO to 0 so that we dont accidentally modify the VAO and VBO we created -> 'unbinding here'
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	glBindVertexArray(0);
-
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
 	while (!glfwWindowShouldClose(window)) {
 
@@ -109,7 +128,7 @@ int main() {
 		glClear(GL_COLOR_BUFFER_BIT);
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO); //tell opengl we wanna use this vao
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawElements(GL_TRIANGLES, 9, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 
 
@@ -120,6 +139,7 @@ int main() {
 
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
 	glDeleteProgram(shaderProgram);
 
 	glfwDestroyWindow(window);
